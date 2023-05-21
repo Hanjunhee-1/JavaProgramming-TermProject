847a8cec9b0fe029d4d09399f0ccce0b8127d635
Hanjunhee-1 <hanjuni857@naver.com>
2023-05-21 19:04:05 +0900

    CardComponent.java 작성

---
+++ CardGame.ver2/src/components/CardComponent.java
@@ -0,0 +1,77 @@
+package components;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class CardComponent extends JPanel{
+	private ImageIcon frontImage;
+	private ImageIcon backImage;
+	private JLabel cardLabel;
+	private boolean isFaceUp;
+	private boolean isMatched;
+	private String cardName;
+	
+	public CardComponent(ImageIcon frontImage, ImageIcon backImage, String fileName) {
+		this.frontImage = frontImage;
+		this.backImage = backImage;
+		isFaceUp = false;
+		isMatched = false;
+		cardName = fileName;
+		
+		cardLabel = new JLabel();
+		cardLabel.setOpaque(false);
+		this.setOpaque(false);
+		setBackImage();
+		setPreferredSize(new Dimension(backImage.getIconWidth(), backImage.getIconHeight()));
+		
+		this.add(cardLabel);
+	}
+	
+	// isFaceUp 필드 getter 
+	public boolean isFaceUp() {
+		return isFaceUp;
+	}
+
+	// isFaceUp 필드 setter
+	public void setFaceUp(boolean isFaceUp) {
+		this.isFaceUp = isFaceUp;
+	}
+	
+	// isMatched 필드 getter 
+	public boolean isMatched() {
+		return isMatched;
+	}
+
+	// isMatched 필드 setter
+	public void setMatched(boolean isMatched) {
+		this.isMatched = isMatched;
+	}
+	
+	// cardName 필드 getter
+	public String getCardName() {
+		return cardName;
+	}
+	
+	// cardName 필드 setter
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	
+	// 카드의 이미지를 앞면으로 설정
+	public void setFrontImage() {
+		cardLabel.setIcon(this.frontImage);
+		
+	}
+	
+	// 카드의 이미지를 뒷면으로 설정
+	public void setBackImage() {
+		cardLabel.setIcon(this.backImage);
+	}
+}
