e214269296a1e3a7854e62bfa6369a4d92aa9bea
Hanjunhee-1 <hanjuni857@naver.com>
2023-05-21 19:04:44 +0900

    Main.java 작성

---
+++ CardGame.ver2/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.io.IOException;
+
+import controller.GUIController;
+
+public class Main {
+	
+	public static void main(String agrs[]) throws IOException {
+		new GUIController();
+	}
+}
