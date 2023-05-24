7bf3edb84bc34a25e40f90fb4f5058e488966b02
Hanjunhee-1 <hanjuni857@naver.com>
2023-05-24 19:51:31 +0900

    PlayerComponent.java 작성

--- CardGame.ver2/src/components/PlayerComponent.java
+++ CardGame.ver2/src/components/PlayerComponent.java
@@ -1,17 +1,32 @@
 package components;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
 /**
  * 
  * 각 플레이어의 점수를 기록하고 그에 관련한 method 를 모아놓은 클래스입니다.
  *
  */
-public class PlayerComponent {
+public class PlayerComponent extends JLabel {
+	private String playerName;
 	private int score;
+	private Color color;
 	
-	public PlayerComponent() {
+	public PlayerComponent(Color color) {
 		score = 0;
 		
-		
+		this.setPreferredSize(new Dimension(200, 800));
+		setColor(color);
+		this.setBackground(color);
+		this.setFont(new Font("Ink Free", Font.BOLD, 20));
+	}
+	
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
 	}
 	
 	public int score() {
@@ -20,5 +35,9 @@ public class PlayerComponent {
 	
 	public void setScore(int score) {
 		this.score = score;
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
 	}
 }
