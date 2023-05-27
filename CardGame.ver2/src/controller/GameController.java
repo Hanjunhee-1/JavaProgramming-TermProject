6a503f656212c16583ca1cd5d17339b7321cf6b4
Hanjunhee-1 <hanjuni857@naver.com>
2023-05-28 01:48:19 +0900

    플레이어 점수 계산 함수 추가

--- CardGame.ver2/src/controller/GameController.java
+++ CardGame.ver2/src/controller/GameController.java
@@ -1,5 +1,14 @@
 package controller;
 
-public class GameController {
+import components.PlayerComponent;
 
+public class GameController {
+	
+	public void calculateScore(boolean isPair, PlayerComponent player) {
+		if (isPair) {
+			player.setScore(player.score() + 1);
+		} else {
+			player.setScore(player.score() > 0 ? player.score() - 1 : player.score());
+		}
+	}
 }
